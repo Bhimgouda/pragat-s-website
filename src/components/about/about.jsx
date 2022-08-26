7e9be747a75fa9d33546e2ff16030a29dbb39832
Bhimgouda <satvikp508@gmail.com>
2022-08-26 23:30:04 +0530

    Added routing and react helmet...but all the other pages are not SEO optimized because of CSR

---
+++ src/components/about/about.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+
+const About = () => {
+  return (
+    <React.Fragment>
+      <Helmet>
+        <title>About Me</title>
+      </Helmet>
+      <h1>About Me</h1>
+    </React.Fragment>
+  );
+};
+
+export default About;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import Form from "./components/common/form";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Form />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
