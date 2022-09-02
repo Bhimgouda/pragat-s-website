c263ee3c43248369bfe79ee14c19b4a2d69ffcd1
Bhimgouda <satvikp508@gmail.com>
2022-09-02 17:51:46 +0530

    naked email address replaced with string

--- src/components/common/form.jsx
+++ src/components/common/form.jsx
@@ -20,7 +20,7 @@ class Form extends Component {
     this.setState({ data: { name: "", message: "", subject: "", email: "" } });
     axios.defaults.headers.post["Content-Type"] = "application/json";
     const response = await axios.post(
-      "https://formsubmit.co/ajax/vyawaharepragat@gmail.com",
+      "https://formsubmit.co/ajax/e4a94ab3178af24c92f756f892be3382",
       {
         name: name,
         email: email,
