7b4324a33e1540bc7c732188de65c7daa2d55136
Bhimgouda <satvikp508@gmail.com>
2022-09-04 10:44:09 +0530

    overflow applied

--- src/components/error.jsx
+++ src/components/error.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Error = () => {
-  return <h1 style={{ textAlign: "center" }}>Error 404</h1>;
+  return (
+    <h1
+      style={{
+        width: "100vw",
+        height: "100vh",
+        textAlign: "center",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      Error 404 - Requested Page Not Found
+    </h1>
+  );
 };
 
 export default Error;
